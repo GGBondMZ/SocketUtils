fa72c8e40c40d4aad366e9b1daa5f95d83bcbc80
mazhuang <mazzmail@163.com>
2023-09-19 09:57:45 +0800

    Initialize project code

---
+++ app/src/main/java/com/devicetools/socketutils/Constant.java
@@ -0,0 +1,12 @@
+package com.devicetools.socketutils;
+
+/**
+ * Created by mz on 2023/09/19.
+ * Time: 09:10
+ * Description:
+ */
+public class Constant {
+    public static final String SERVER_TEXT = "XJYD_SERVER_TEXT";//[88, 74, 89, 68, 95, 83, 69, 82, 86, 69, 82, 95, 84, 69, 88, 84]
+    public static final String SERVER_FILE = "XJYD_SERVER_FILE";//[88, 74, 89, 68, 95, 83, 69, 82, 86, 69, 82, 95, 70, 73, 76, 69]
+    public static final int SERVER_TYPE = SERVER_FILE.length();
+}
---
+++ app/src/main/java/com/devicetools/socketutils/MainActivity.java
@@ -0,0 +1,47 @@
+package com.devicetools.socketutils;
+
+import android.Manifest;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.RuntimePermissions;
+
+@RuntimePermissions
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTvIp;
+    private Button mBt1;
+    private Button mBt2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mTvIp = findViewById(R.id.tv_ip);
+        mBt1 = findViewById(R.id.bt1);
+        mBt2 = findViewById(R.id.bt2);
+        mBt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ServerActivity.class));//服务器
+            }
+        });
+        mBt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ClientActivity.class));//客户端
+            }
+        });
+    }
+
+    @NeedsPermission({Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE})
+    void needsPermission() {
+    }
+
+}
